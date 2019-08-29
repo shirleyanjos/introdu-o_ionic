0b7029a1b97e23d0f167b72a2f189a223bdff53a
shirley <shirleyanjos2018@gmail.com>
2019-08-29 11:29:56 -0300

    primeiro commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,44 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  nome: string = "Shirley" ;
+  cor = "primary";
+  humor = "sad"
+  checked_humor = false;
+
   constructor() {}
 
+  trocarCor():void{
+    console.log('Chamou trocarCor!')
+    if (this.cor == "primary") {
+      this.cor = "success";
+    } 
+    else if (this.cor == "success") {
+    this.cor = "danger";
+
+    } 
+    else if (this.cor == "danger") {
+      this.cor = "warning";
+  
+    }
+    else if (this.cor == "warning") {
+      this.cor = "dark";
+  
+    }else {
+    this.cor = "primary";
+    }
+  }
+  mudaHumor(): void{
+    console.log('Chamou mudaHumor');
+    this.checked_humor = !this.checked_humor;
+    if (this.checked_humor == true){
+      this.humor = "happy";
+    } else {
+      this.humor = "sad";
+    }
+
+  }
+   
 }
+
+
